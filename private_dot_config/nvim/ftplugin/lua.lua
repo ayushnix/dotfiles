95a1d04736709366e961a178021e6c245585fd95
Ayush Agarwal <ayushnix@fastmail.com>
2022-04-21 19:51:45 +0530

    enable code folding

--- private_dot_config/nvim/ftplugin/lua.lua
+++ private_dot_config/nvim/ftplugin/lua.lua
@@ -1,7 +1,3 @@
-vim.bo.shiftwidth = 2
-vim.bo.tabstop = 2
-vim.bo.softtabstop = 2
-vim.bo.expandtab = true
--- code folding using treesitter
 vim.wo.foldmethod = 'expr'
 vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.wo.foldlevel = 99
